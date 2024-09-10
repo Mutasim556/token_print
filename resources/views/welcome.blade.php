8fc8f2c47b4319a8993ae971f35cbb4327778286
Mutasim556 <134157496+Mutasim556@users.noreply.github.com>
2024-09-10 09:27:38 +0600

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -236,8 +236,7 @@
                 </div> --}}
         </div>
     </div>
-    <script src="{{ asset('public/jquery/jquery-3.7.1.min.js') }}"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="{{ asset('public/jquery/jquery-3.7.1.min.js') }}"></script>
     {{-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> --}}
     <script src="{{ asset('public/bootstrap/js/bootstrap.bundle.min.js') }}"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
@@ -262,7 +261,6 @@
             })
         }
         $(document).ready(function() {
-            $('.js-example-basic-single').select2();
             setInterval(getCurrentMealInfo, 3000);
         });
     </script>
